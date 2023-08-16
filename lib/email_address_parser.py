d8a2d887c0f431254ec8a91390d8637984edfc90
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-15 21:05:35 -0400

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,9 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    
+    def parse(self):
+        regex = re.compile(r'[\s|\,]{1,2}')
+        return [email for email in sorted(regex.split(self.email_addresses)) if '@' in email]
